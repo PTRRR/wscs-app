162973da905af4db86f2161b69b043b3531c8c93
PTRRR <pietro.alberti@bluewin.ch>
2024-10-22 11:51:16 +0200

    Require login before checkout

--- src/routes/(app)/checkout/+page.svelte
+++ src/routes/(app)/checkout/+page.svelte
@@ -80,77 +80,83 @@
 
 <h1>Checkout</h1>
 
-{#if products.length === 0 && !loadingCart}
-	<span>Nothing in the cart</span>
-{:else}
-	{#each products as product}
-		{#key product.id}
-			<div class="cart-item">
-				<a href={`/products/${product.id}`}>{product.title}</a>
-				{#each getVariationsForProduct(product) as variation}
-					{#key variation.id}
-						<p>{variation.name}</p>
-						<button
-							on:click={() => removeFromCart({ product: product.id, variation: variation.id })}
-						>
-							Remove from cart
-						</button>
-					{/key}
-				{/each}
-				<p>Price: {getProductTotal(product)}€</p>
-			</div>
-		{/key}
-	{/each}
-{/if}
+{#if !$user.data?.user && $user.isFetched}
+	<p>Please <a href="/login">login</a> in order to checkout</p>
+{:else if $user.isFetched}
+	{#if products.length === 0 && !loadingCart}
+		<span>Nothing in the cart</span>
+	{:else}
+		{#each products as product}
+			{#key product.id}
+				<div class="cart-item">
+					<a href={`/products/${product.id}`}>{product.title}</a>
+					{#each getVariationsForProduct(product) as variation}
+						{#key variation.id}
+							<p>{variation.name}</p>
+							<button
+								on:click={() => removeFromCart({ product: product.id, variation: variation.id })}
+							>
+								Remove from cart
+							</button>
+						{/key}
+					{/each}
+					<p>Price: {getProductTotal(product)}€</p>
+				</div>
+			{/key}
+		{/each}
+	{/if}
 
-{#if loadingCart}
-	<span>Loading cart...</span>
-{:else}
-	<p>TOTAL: {total}€</p>
-{/if}
+	{#if loadingCart}
+		<span>Loading cart...</span>
+	{:else}
+		<p>TOTAL: {total}€</p>
+	{/if}
 
-{#if stripe && clientSecret && !processing}
-	<form
-		on:submit|preventDefault={async () => {
-			try {
-				const userData = $user.data?.user;
-				if (stripe && elements && userData) {
-					const result = await stripe.confirmPayment({
-						elements,
-						redirect: 'if_required'
-					});
+	{#if stripe && clientSecret && !processing}
+		<form
+			on:submit|preventDefault={async () => {
+				try {
+					const userData = $user.data?.user;
+					if (stripe && elements && userData) {
+						const result = await stripe.confirmPayment({
+							elements,
+							redirect: 'if_required'
+						});
 
-					if (result.error) {
-						console.log(result.error);
-						return;
-					}
+						if (result.error) {
+							console.log(result.error);
+							return;
+						}
 
-					if (result.paymentIntent?.status === 'succeeded') {
-						$cart = { items: [] };
-						goto('/payment-confirmation');
-					}
-				}
-			} catch (error) {
-				console.log(error);
-			} finally {
-				processing = false;
-			}
-		}}
-	>
-		<Elements {stripe} {clientSecret} theme="flat" bind:elements>
-			<LinkAuthenticationElement />
-			<PaymentElement
-				options={{
-					defaultValues: {
-						billingDetails: {
-							email: $user.data?.user?.email
+						if (result.paymentIntent?.status === 'succeeded') {
+							$cart = { items: [] };
+							goto('/payment-confirmation');
 						}
 					}
-				}}
-			/>
-		</Elements>
-		<button type="submit">Pay</button>
-	</form>
-{:else if stripe && clientSecret && !processing}
-	<p>Processing payment...</p>
+				} catch (error) {
+					console.log(error);
+				} finally {
+					processing = false;
+				}
+			}}
+		>
+			<Elements {stripe} {clientSecret} theme="flat" bind:elements>
+				<LinkAuthenticationElement />
+				<PaymentElement
+					options={{
+						defaultValues: {
+							billingDetails: {
+								email: $user.data?.user?.email
+							}
+						}
+					}}
+				/>
+			</Elements>
+			<button type="submit">Pay</button>
+		</form>
+	{:else if stripe && clientSecret && !processing}
+		<p>Processing payment...</p>
+	{/if}
+{:else}
+	<p>Loading...</p>
 {/if}
