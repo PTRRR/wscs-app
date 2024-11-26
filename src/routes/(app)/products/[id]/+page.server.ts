388fbc17f43d6d2aafba3f703231d3808c2f50cc
PTRRR <pietro.alberti@bluewin.ch>
2024-11-26 17:42:08 +0100

    Implement error page

--- src/routes/(app)/products/[id]/+page.server.ts
+++ src/routes/(app)/products/[id]/+page.server.ts
@@ -1,5 +1,6 @@
 import { WSCS } from '../../../../utilities/api';
 import type { PageServerLoad } from './$types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params, parent }) => {
 	const {
@@ -7,18 +8,24 @@ export const load: PageServerLoad = async ({ params, parent }) => {
 	} = await parent();
 
 	const api = new WSCS(baseUrl);
-	const [product, variations] = await Promise.all([
-		api.findProductById(params.id),
-		api.findVariations({
-			query: {
-				product: {
-					equals: params.id
-				}
-			}
-		})
-	]);
 
-	return {
-		product: { ...product, variations }
-	};
+	try {
+		const [product, variations] = await Promise.all([
+			api.findProductById(params.id),
+			api.findVariations({
+				query: {
+					product: {
+						equals: params.id
+					}
+				}
+			})
+		]);
+
+		return {
+			product: { ...product, variations }
+		};
+	} catch (e) {
+		console.log(e);
+		return error(404, 'Product not found');
+	}
 };
---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	console.log($page);
+</script>
+
+<div class="error">
+	<h4>Error</h4>
+	<p>{$page.error?.message || 'Unknown error'}</p>
+	<a href="/">Home</a>
+</div>
+
+<style lang="scss">
+	.error {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		h4 {
+			font-size: 2rem;
+		}
+
+		h4,
+		p {
+			margin: 0;
+		}
+
+		a {
+			margin-top: 2rem;
+		}
+	}
+</style>
