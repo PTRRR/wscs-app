3f704096b97761ec4c9e923a5e82f4f999b18faf
PTRRR <pietro.alberti@bluewin.ch>
2024-11-30 14:23:41 +0100

    Update cdn config

--- sst.config.ts
+++ sst.config.ts
@@ -23,6 +23,75 @@ export default $config({
 				TYPESENSE_PORT: process.env.TYPESENSE_PORT as string,
 				TYPESENSE_PROTOCOL: process.env.TYPESENSE_PROTOCOL as string,
 				TYPESENSE_PRODUCTS_COLLECTION: process.env.TYPESENSE_PRODUCTS_COLLECTIO as string
+			},
+			transform: {
+				cdn: (args) => {
+					if (Array.isArray(args.origins)) {
+						args.origins.push({
+							originId: 'payload-origin',
+							domainName: 'cm20hjsx1000008jre1ya33xb.vpr-group.ch',
+							customOriginConfig: {
+								httpPort: 80,
+								originProtocolPolicy: 'http-only',
+								httpsPort: 443,
+								originSslProtocols: ['TLSv1.2']
+							}
+						});
+					}
+
+					if (Array.isArray(args.orderedCacheBehaviors)) {
+						args.orderedCacheBehaviors.push({
+							pathPattern: 'admin*',
+							targetOriginId: 'payload-origin',
+							viewerProtocolPolicy: 'redirect-to-https',
+							allowedMethods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'POST', 'PATCH', 'DELETE'],
+							cachedMethods: ['GET', 'HEAD'],
+							compress: true,
+							defaultTtl: 0,
+							minTtl: 0,
+							maxTtl: 0,
+							forwardedValues: {
+								cookies: { forward: 'all' },
+								headers: ['*'],
+								queryString: true
+							}
+						});
+
+						args.orderedCacheBehaviors.push({
+							pathPattern: '_next*',
+							targetOriginId: 'payload-origin',
+							viewerProtocolPolicy: 'redirect-to-https',
+							allowedMethods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'POST', 'PATCH', 'DELETE'],
+							cachedMethods: ['GET', 'HEAD'],
+							compress: true,
+							defaultTtl: 0,
+							minTtl: 0,
+							maxTtl: 0,
+							forwardedValues: {
+								cookies: { forward: 'all' },
+								headers: ['*'],
+								queryString: true
+							}
+						});
+
+						args.orderedCacheBehaviors.push({
+							pathPattern: 'api*',
+							targetOriginId: 'payload-origin',
+							viewerProtocolPolicy: 'redirect-to-https',
+							allowedMethods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'POST', 'PATCH', 'DELETE'],
+							cachedMethods: ['GET', 'HEAD'],
+							compress: true,
+							defaultTtl: 0,
+							minTtl: 0,
+							maxTtl: 0,
+							forwardedValues: {
+								cookies: { forward: 'all' },
+								headers: ['*'],
+								queryString: true
+							}
+						});
+					}
+				}
 			}
 		});
 	}
